45076ef0193b19cd94e60f1a37ddeb7cf3337928
Rmassey11 <ronmassey11@gmail.com>
2022-08-15 11:29:20 -0400

    JS written
    JS written for Reverse
    html and css files adjusted for display

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//Get the string from the page
+//Controller Function
+function getString(){
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+//Reverse the string
+//Logic Function
+function reverseString(userString){
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString
+}
+
+//Display the reversed string to the user
+//View Function
+function displayString(revstring){
+    //write message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revstring}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
